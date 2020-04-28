a2aa6d485a8b716c11391e59de335c040b9905fb
mlisivenko <lisivenkoma@sibur.ru>
2020-04-28 13:24:40 +0300

    button applyed

---
+++ Assets/ColorPickerTriangleNew/Scripts/ColorSetter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorSetter : MonoBehaviour
+{
+
+    private Text[] myItems;
+    private ColorPickerTriangle CP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("ColorSetter started ");
+
+        myItems = FindObjectsOfType(typeof(Text)) as Text[];
+        foreach (Text item in myItems)
+        {
+            Debug.Log(item.gameObject.name);
+        }
+
+        CP = ColoringObject.GetComponent<ColorPickerTriangle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
